1d77a1cb8c85db254033a69d146121d6c11b45a3
lauta-dev <altairdiaz245@gmail.com>
2024-09-23 19:44:16 -0300

    feat: Show csv output in table

---
+++ src/components/Table.tsx
@@ -0,0 +1,43 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHeader,
+	TableRow,
+	TableHead,
+} from "@/components/ui/table";
+
+export default function TableSte({
+	header,
+	content,
+}: {
+	header: string[];
+	content: string[];
+}) {
+	return (
+		<div className="overflow-y-auto max-h-auto">
+			<Table className="min-w-full relative">
+				<TableHeader>
+					<TableRow>
+						{header.map((header) => (
+							<TableHead className="sticky top-0" key={header}>
+								{header}
+							</TableHead>
+						))}
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{content.map((row, rowIndex) => (
+						<TableRow key={rowIndex}>
+							{header.map((column, colIndex) => (
+								<TableCell key={`${rowIndex}-${colIndex}`}>
+									<p className="text-nowrap">{row[column]}</p>
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</div>
+	);
+}
